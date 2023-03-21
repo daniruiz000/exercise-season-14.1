fef9e1579c4494f312b25fc79fe5dc4519c3eb0d
Daniel Ruiz <danielruimar@gmail.com>
2023-03-21 16:41:13 +0100

    add exercise season 14.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
+import RegisterForm from './components/RegisterForm/RegisterForm';
+import { IntlProvider, FormattedMessage } from 'react-intl';
+import Spanish from './lang/es.json';
+import French from './lang/fr.json';
+import English from './lang/en.json';
 
-function App() {
+const locale = navigator.language;
+let defaultMessages;
+
+switch (locale) {
+  case 'fr-FR':
+    defaultMessages = French;
+
+    break;
+  case 'es-ES':
+    defaultMessages = Spanish;
+
+    break;
+  case 'en-EN':
+    defaultMessages = English;
+
+    break;
+
+  default:
+    defaultMessages = English;
+
+    break;
+}
+const App = () => {
+  const [messages, setMessages] = React.useState(defaultMessages);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <IntlProvider locale={locale} messages={messages}>
+        <RegisterForm />
+        <h2><FormattedMessage id='app:language_selector' /></h2>
+        <button onClick={() => setMessages(Spanish)}><FormattedMessage id='app:spanish' /></button>
+        <button onClick={() => setMessages(French)}><FormattedMessage id='app:french' /></button>
+        <button onClick={() => setMessages(English)}><FormattedMessage id='app:english' /></button>
+      </IntlProvider>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/RegisterForm/RegisterForm.js
@@ -0,0 +1,53 @@
+import { useForm } from 'react-hook-form';
+import React from 'react';
+import './RegisterForm.scss';
+import { FormattedMessage, useIntl } from 'react-intl';
+
+const RegisterForm = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const [formData, setFormData] = React.useState(null);
+
+  const onSubmit = (data) => {
+    setFormData(data);
+  };
+  const { formatMessage } = useIntl();
+
+  return (
+    <div className='register-form'>
+      <h2><FormattedMessage id='register_form:register_title' /></h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <fieldset className='register-form__fieldset'>
+          <input placeholder={formatMessage({ id: 'register_form:first_name' })} type='text' {...register('firstName', { required: true })} />
+          {errors?.firstName && (
+            <p className='register-form__error'>
+              <FormattedMessage id='register_form:required_field' />
+            </p>
+          )}
+        </fieldset>
+        <fieldset className='register-form__fieldset'>
+          <input placeholder={formatMessage({ id: 'register_form:last_name' })} type='text' {...register('lastName', { required: true, minLength: 3 })} />
+          {errors?.lastName && (
+            <p className='register-form__error'>
+              <FormattedMessage id='register_form:required_field_3_chars' />
+            </p>
+          )}
+        </fieldset>
+        <button type='submit'><FormattedMessage id='register_form:submit_button' /></button>
+      </form>
+      <h2><FormattedMessage id='register_form:form_data_title' /></h2>
+      <p><FormattedMessage id='register_form:first_name' />: {formData?.firstName}</p>
+      <p><FormattedMessage id='register_form:last_name' />: {formData?.lastName}</p>
+
+      <h2><FormattedMessage id='register_form:live_data_title' /></h2>
+      <p><FormattedMessage id='register_form:first_name' />: {watch('firstName')}</p>
+      <p><FormattedMessage id='register_form:last_name' />: {watch('lastName')}</p>
+    </div>
+  );
+};
+
+export default RegisterForm;
